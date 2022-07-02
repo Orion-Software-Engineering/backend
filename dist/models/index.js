1ffd6f852f62086fd3080249ff569aa4c51b7781
Max <maxotuteye12@gmail.com>
2022-07-02 18:14:04 +0000

    added models

--- dist/models/index.js
+++ dist/models/index.js
@@ -1,1 +1,40 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sequelize_1 = require("sequelize");
+const config = require('../config/db.config');
+const sequelize = new sequelize_1.Sequelize(config.DB, config.USER, config.PASSWORD, {
+    host: config.HOST,
+    dialect: config.dialect,
+    // @ts-ignore
+    operatorsAliases: false,
+    pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle,
+    }
+});
+const db = {};
+// @ts-ignore
+db.Sequelize = sequelize_1.Sequelize;
+// @ts-ignore
+db.sequelize = sequelize;
+// @ts-ignore
+db.user = require("../models/user.model.js")(sequelize, sequelize_1.Sequelize);
+// @ts-ignore
+db.role = require("../models/role.model.js")(sequelize, sequelize_1.Sequelize);
+// @ts-ignore
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+// @ts-ignore
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+// @ts-ignore
+db.ROLES = ["user", "admin", "moderator"];
+module.exports = db;
---
+++ models/index.ts
@@ -0,0 +1,45 @@
+import {Sequelize} from 'sequelize'
+
+const config = require('../config/db.config');
+const sequelize = new Sequelize(
+    config.DB,
+    config.USER,
+    config.PASSWORD,
+    {
+        host: config.HOST,
+        dialect: config.dialect,
+        // @ts-ignore
+        operatorsAliases: false,
+        pool: {
+            max: config.pool.max,
+            min: config.pool.min,
+            acquire: config.pool.acquire,
+            idle: config.pool.idle,
+        }
+    }
+)
+
+const db = {};
+// @ts-ignore
+db.Sequelize = Sequelize;
+// @ts-ignore
+db.sequelize = sequelize;
+// @ts-ignore
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+// @ts-ignore
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+// @ts-ignore
+db.role.belongsToMany(db.user, {
+    through: "user_roles",
+    foreignKey: "roleId",
+    otherKey: "userId"
+});
+// @ts-ignore
+db.user.belongsToMany(db.role, {
+    through: "user_roles",
+    foreignKey: "userId",
+    otherKey: "roleId"
+});
+// @ts-ignore
+db.ROLES = ["user", "admin", "moderator"];
+module.exports = db;
