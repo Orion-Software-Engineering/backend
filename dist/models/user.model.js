3de42f1ce933d84ed9a97b570f5bf3ebb40ee7d5
Max <maxotuteye12@gmail.com>
2022-07-02 17:45:59 +0000

    adding user model

--- dist/models/user.model.js
+++ dist/models/user.model.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+module.exports = (sequelize, Sequelize) => {
+    // TODO: Address sequelize types
+    return sequelize.define('users', {
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    });
+};
---
+++ models/user.model.ts
@@ -0,0 +1,16 @@
+import sequelize, {DataTypes} from "sequelize";
+
+module.exports = (sequelize: sequelize.Sequelize, Sequelize: DataTypes) => {
+    // TODO: Address sequelize types
+    return sequelize.define('users', {
+        username: {
+            type: Sequelize.STRING
+        },
+        email: {
+            type: Sequelize.STRING
+        },
+        password: {
+            type: Sequelize.STRING
+        }
+    })
+}
