27a26699ccffb88b493b384760a10d45e9df3fc1
Max <maxotuteye12@gmail.com>
2022-07-03 16:15:22 +0000

    sequelize stuff

---
+++ migrations/20220703161010-create-user-roles.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('user_roles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      roleId: {
+        type: Sequelize.NUMBER
+      },
+      userId: {
+        type: Sequelize.NUMBER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('user_roles');
+  }
+};
