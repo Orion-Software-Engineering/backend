ffa59333fe91175d59ddd57f832bc66631abf8a7
Max <maxotuteye12@gmail.com>
2022-07-03 10:00:43 +0000

    changed strict to false

--- dist/middleware/authJwt.js
+++ dist/middleware/authJwt.js
@@ -6,4 +6,18 @@ const db = require('../models');
 const User = db.user;
 const verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided"
+        });
+    }
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: "Unauthorized"
+            });
+        }
+        req.userId = decoded.id;
+        next();
+    });
 };
--- middleware/authJwt.ts
+++ middleware/authJwt.ts
@@ -7,4 +7,20 @@ const User = db.user
 
 const verifyToken = (req: Request, res: Response, next: Function) => {
     let token = req.headers["x-access-token"]
+
+    if (!token) {
+        return res.status(403).send({
+            message: "No token provided"
+        })
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({
+                message:"Unauthorized"
+            })
+        }
+        req.userId = decoded.id
+        next()
+    })
 }
