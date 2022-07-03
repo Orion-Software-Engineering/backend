980417e5e7af0c341a4145f09445ea837358b9e0
Max <maxotuteye12@gmail.com>
2022-07-03 13:59:50 +0000

    finished authorization controller

--- dist/controller/auth.controller.js
+++ dist/controller/auth.controller.js
@@ -44,6 +44,34 @@ exports.signin = (req, res) => {
         }
     }).then(user => {
         if (!user) {
+            return res.status(404).send({ message: "User not found" });
         }
+        const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if (!passwordIsValid) {
+            return res.status(401).send({
+                accessToken: null,
+                message: "Invalid Password."
+            });
+        }
+        const token = jwt.sign({ id: user.id }, config.secret, {
+            expiresIn: 5184000
+        });
+        const authorities = [];
+        user.getRoles().then(roles => {
+            for (let i = 0; i < roles.length; i++) {
+                authorities.push("ROLE_" + roles[i].name.toUpperCase());
+            }
+            res.status(200).send({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                roles: authorities,
+                accessToken: token
+            });
+        });
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message
+        });
     });
 };
