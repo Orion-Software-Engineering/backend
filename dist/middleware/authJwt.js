a75204a3c03df6aed954513e3cc9cf982f532816
Max <maxotuteye12@gmail.com>
2022-07-04 01:39:12 +0000

    adding comments against all odds

--- dist/middleware/authJwt.js
+++ dist/middleware/authJwt.js
@@ -5,23 +5,28 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/auth.config');
 const db = require('../models');
 const User = db.user;
+// various modules for access checks with token verification
 const verifyToken = (req, res, next) => {
     let token = req.headers["x-access-token"];
+    // get token from request and return if null
     if (!token) {
         return res.status(403).send({
             message: "No token provided"
         });
     }
-    jwt.verify(token, config.secret, (err, decoded) => {
-        if (err) {
-            return res.status(401).send({
-                message: "Unauthorized"
-            });
-        }
-        // @ts-ignore
-        req.userId = decoded.id;
-        next();
-    });
+    // check that the token is actually a string else JWT will cry
+    if (typeof token === "string") {
+        jwt.verify(token, config.secret, (err, decoded) => {
+            if (err) {
+                return res.status(401).send({
+                    message: "Unauthorized"
+                });
+            }
+            // @ts-ignore
+            req.userId = decoded.id; // add a new entry called userId, needed later
+            next();
+        });
+    }
 };
 const isAdmin = (req, res, next) => {
     // @ts-ignore
--- middleware/authJwt.ts
+++ middleware/authJwt.ts
@@ -5,25 +5,30 @@ const config = require('../config/auth.config')
 const db = require('../models')
 const User = db.user
 
+// various modules for access checks with token verification
+
 const verifyToken = (req: Request, res: Response, next: Function) => {
     let token = req.headers["x-access-token"]
-
+    // get token from request and return if null
     if (!token) {
         return res.status(403).send({
             message: "No token provided"
         })
     }
 
-    jwt.verify(token, config.secret, (err: any, decoded: any) => {
-        if (err) {
-            return res.status(401).send({
-                message: "Unauthorized"
-            })
-        }
-        // @ts-ignore
-        req.userId = decoded.id
-        next()
-    })
+    // check that the token is actually a string else JWT will cry
+    if (typeof token === "string") {
+        jwt.verify(token, config.secret, (err: any, decoded: any) => {
+            if (err) {
+                return res.status(401).send({
+                    message: "Unauthorized"
+                })
+            }
+            // @ts-ignore
+            req.userId = decoded.id     // add a new entry called userId, needed later
+            next()
+        })
+    }
 }
 
 const isAdmin = (req: Request, res: Response, next: Function) => {
