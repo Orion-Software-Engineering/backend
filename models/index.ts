c09db776039b850dcc212ebbd28ed607ad584af1
Max <maxotuteye12@gmail.com>
2022-07-04 01:55:20 +0000

    adding comments for later

--- models/index.ts
+++ models/index.ts
@@ -1,6 +1,12 @@
 import {Sequelize} from 'sequelize'
 
 const config = require('../config/db.config');
+
+/* Heroku essential
+* Heroku provides a database url, but locally we do not need that.
+* If testing locally, we use our config
+* else we use the provided database url (it changes so do not hardcode the value!)
+* */
 const sequelize = process.env.DATABASE_URL ?
     new Sequelize(process.env.DATABASE_URL,
         {
@@ -30,6 +36,7 @@ const sequelize = process.env.DATABASE_URL ?
         }
     )
 
+// the db variable will store database info for use
 const db = {};
 // @ts-ignore
 db.Sequelize = Sequelize;
@@ -41,24 +48,32 @@ db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
 // @ts-ignore
 db.interest = require("../models/interest.model.js")(sequelize, Sequelize)
+
+// one role can have many users
 // @ts-ignore
 db.role.belongsToMany(db.user, {
     through: "user_roles",
     foreignKey: "roleId",
     otherKey: "userId"
 });
+
+// one user can have multiple roles
 // @ts-ignore
 db.user.belongsToMany(db.role, {
     through: "user_roles",
     foreignKey: "userId",
     otherKey: "roleId"
 });
+
+// one interest can have multiple users
 // @ts-ignore
 db.interest.belongsToMany(db.user, {
     through: "user_interests",
     foreignKey: "userId",
     otherKey: "roleId"
 })
+
+// one user can have multiple interests
 //@ts-ignore
 db.user.belongsToMany(db.interest, {
     through: "user_interests",
