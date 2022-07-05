6f6644341801dcc5bf3416f8bcd6e3e191cacd1c
Max <maxotuteye12@gmail.com>
2022-07-05 21:45:13 +0000

    fixing issues after refactoring

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,28 +1,29 @@
 import {Request, Response} from 'express';
 import db from '../models';
+
 const {User} = db;
 
 // basic controllers for content
 export const allAccess = (req: Request, res: Response) => {
-  res.status(200).send('Public Content');
+    res.status(200).send('Public Content');
 };
 
 export const userBoard = (req: Request, res: Response) => {
-  res.status(200).send('User Content.');
+    res.status(200).send('User Content.');
 };
 
 export const adminBoard = (req: Request, res: Response) => {
-  res.status(200).send('Admin Content.');
+    res.status(200).send('Admin Content.');
 };
 
 export const moderatorBoard = (req: Request, res: Response) => {
-  res.status(200).send('Moderator Content.');
+    res.status(200).send('Moderator Content.');
 };
 
 // show all registered users
 export const showAll = async (req: Request, res: Response) => {
-  const users = await User.findAll();
-  res.status(200).send({
-    users,
-  });
+    await User.findAll()
+        .then(users => {
+            res.status(200).send({users})
+        });
 };
--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -5,50 +5,48 @@ import {InterestAttributes} from '../interest';
 import {UserAttributes, UserCreationAttributes} from './user.type';
 
 // define database model for users
-export default class User extends Model<
-  UserAttributes,
-  UserCreationAttributes
-> {
-  declare id: CreationOptional<string>;
-  declare username: string;
-  declare email: string;
-  declare password: string;
-  declare isEmailVerified: boolean;
-  declare getRoles: Sequelize.BelongsToManyGetAssociationsMixin<RoleAttributes>;
-  declare setRoles: Sequelize.BelongsToManySetAssociationsMixin<
-    RoleAttributes,
-    RoleAttributes['id']
-  >;
-  declare getInterests: Sequelize.BelongsToManyGetAssociationsMixin<InterestAttributes>;
-  declare setInterests: Sequelize.BelongsToManySetAssociationsMixin<
-    InterestAttributes,
-    InterestAttributes['id']
-  >;
+export default class User extends Model<UserAttributes,
+    UserCreationAttributes> {
+    declare id: CreationOptional<string>;
+    declare username: string;
+    declare email: string;
+    declare password: string;
+    declare isEmailVerified: boolean;
+    declare getRoles: Sequelize.BelongsToManyGetAssociationsMixin<RoleAttributes>;
+    declare setRoles: Sequelize.BelongsToManySetAssociationsMixin<RoleAttributes,
+        RoleAttributes['id']>;
+    declare getInterests: Sequelize.BelongsToManyGetAssociationsMixin<InterestAttributes>;
+    declare setInterests: Sequelize.BelongsToManySetAssociationsMixin<InterestAttributes,
+        InterestAttributes['id']>;
 }
 
 User.init(
-  {
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
+    {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        isEmailVerified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
     },
-    username: {
-      type: DataTypes.STRING,
-    },
-    email: {
-      type: DataTypes.STRING,
-    },
-    password: {
-      type: DataTypes.STRING,
-    },
-    isEmailVerified: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'users',
-  }
+    {
+        sequelize,
+        tableName: 'users',
+    }
 );
