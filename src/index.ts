bb3bebcc6d6d6edcd9cbc66d0378a901a7b3f920
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-06 23:28:29 +0000

    add interest routes to app

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
-import verifyEmailRoutes from './routes/verify.routes'
+import verifyEmailRoutes from './routes/verify.routes';
 import db, {sequelize} from './models';
+import interestRouter from './routes/interest.routes';
 
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const corsOptions = {
-    origin: 'https://localhost:8000',
+  origin: 'https://localhost:8000',
 };
 
 app.use(cors(corsOptions));
@@ -20,53 +21,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 const {Role, Interest, INTERESTS, ROLES} = db;
 
 sequelize
-    .sync({force: true}) // force: true forces dropping and resyncing the database
-    .then(() => {
-        console.log('Syncing DB');
-        initial()
-    });
+  .sync({force: true}) // force: true forces dropping and resyncing the database
+  .then(() => {
+    console.log('Syncing DB');
+    initial();
+  });
 
 // this function initializes the roles, run only once on a new database else there'll be errors
 function initial() {
-    // Role.create({
-    //     id: 1,
-    //     name: "user"
-    // });
-    //
-    // Role.create({
-    //     id: 2,
-    //     name: "moderator"
-    // });
-    //
-    // Role.create({
-    //     id: 3,
-    //     name: "admin"
-    // });
-
-    ROLES.forEach(role => {
-        Role.create({
-            name: role,
-        });
+  ROLES.forEach(role => {
+    Role.create({
+      name: role,
     });
+  });
 
-    INTERESTS.forEach(interest => {
-        Interest.create({
-            name: interest,
-        });
+  INTERESTS.forEach(interest => {
+    Interest.create({
+      name: interest,
     });
+  });
 }
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({message: 'Welcome to Orion Meet'});
+  res.json({message: 'Welcome to Orion Meet'});
 });
 
 userRoutes(app);
 authRoutes(app);
-verifyEmailRoutes(app)
+verifyEmailRoutes(app);
+app.use('/api/test/interest', interestRouter);
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-    console.log(`DATABASE_URL is ${process.env.DATABASE_URL}`);
+  console.log(`Server running on port ${PORT}`);
+  console.log(`DATABASE_URL is ${process.env.DATABASE_URL}`);
 });
