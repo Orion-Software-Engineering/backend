b6621b3d59887e980193810d71a63309a31f9698
Max <maxotuteye12@gmail.com>
2022-07-09 18:19:46 +0000

    refactored check roles

--- src/middleware/sign_up/checkRolesExist.ts
+++ src/middleware/sign_up/checkRolesExist.ts
@@ -5,12 +5,16 @@ const {ROLES} = db;
 
 const checkRolesExist = (req: Request, res: Response, next: Function) => {
     if (req.body.roles) {
-        req.body.roles.forEach((role: string) => {
-            if (!ROLES.includes(role)) {
-                return res.status(400).send({
-                    'message': `Role ${role} does not exist.`
-                });
-            }
+        if (Array.isArray(req.body.roles))
+            req.body.roles.forEach((role: string) => {
+                if (!ROLES.includes(role)) {
+                    return res.status(400).send({
+                        'message': `Role ${role} does not exist.`
+                    });
+                }
+            })
+        else return res.status(400).send({
+            'message': 'Roles must be an array'
         })
     }
     next()
