b7b20134a6a40fcab6a0e105952950d5fb448dfa
Max <maxotuteye12@gmail.com>
2022-07-09 17:59:30 +0000

    refactoring check interests

--- src/middleware/sign_up/checkInterestsExist.ts
+++ src/middleware/sign_up/checkInterestsExist.ts
@@ -5,14 +5,21 @@ const {INTERESTS} = db;
 
 const checkInterestsExist = (req: Request, res: Response, next: Function) => {
     if (req.body.interests) {
-        for (let i = 0; i < req.body.interests.length; i++) {
-            if (!INTERESTS.includes(req.body.interests[i])) {
-                res.status(400).send({
-                    message: 'Interests does not exist',
-                });
-                return;
+        req.body.interests.forEach((interest: string) => {
+            if (!INTERESTS.includes(interest)) {
+                res.status(400).send(`Interest ${interest} does not exist`);
             }
-        }
+            return
+        })
+
+        // for (let i = 0; i < req.body.interests.length; i++) {
+        //     if (!INTERESTS.includes(req.body.interests[i])) {
+        //         res.status(400).send({
+        //             message: 'Interests does not exist',
+        //         });
+        //         return;
+        //     }
+        // }
     }
     next();
 };
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,14 +1,14 @@
-import {Express, NextFunction, Request, Response} from 'express';
-
-import {verifySignUp} from '../middleware/verifySignUp';
+import {Express, Request, Response} from 'express';
 
 import * as controller from '../controller/auth.controller';
-import {authJwt} from "../middleware/authJwt";
+import checkDuplicatedUsernameOrEmail from "../middleware/sign_up/checkDuplicatedUsernameOrEmail";
+import checkRolesExist from "../middleware/sign_up/checkRolesExist";
+import isUserVerified from "../middleware/authentication/isUserVerified";
 
 // routes for authentication functions
 
 export default (app: Express) => {
-    app.use((req: Request, res: Response, next: NextFunction) => {
+    app.use((req: Request, res: Response, next: Function) => {
         res.header(
             'Access-Control-Allow-Headers',
             'x-access-token, Origin, Content-Type, Accept'
@@ -20,14 +20,14 @@ export default (app: Express) => {
     app.post(
         '/api/auth/signup',
         [
-            verifySignUp.checkDuplicatedUsernameOrEmail,
-            verifySignUp.checkRolesExisted,
+            checkDuplicatedUsernameOrEmail,
+            checkRolesExist,
         ],
         controller.signup
     );
 
     // the sign in controller
     app.post('/api/auth/signin',
-        [authJwt.isUserVerified],
+        [isUserVerified],
         controller.signin);
 };
