ef230e72fdc6282fc19bc28413e549ad959e0de2
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-16 13:45:42 +0000

    add message model

---
+++ src/models/message/message.model.ts
@@ -0,0 +1,26 @@
+import {CreationOptional, DataTypes, Model} from 'sequelize';
+import {sequelize} from '..';
+import {MessageAttributes, MessageCreationAttributes} from './message.type';
+
+export default class Message extends Model<
+  MessageAttributes,
+  MessageCreationAttributes
+> {
+  declare id: CreationOptional<string>;
+  declare idHash: string;
+  declare text: string;
+}
+
+Message.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    idHash: {type: DataTypes.STRING, allowNull: false},
+    text: {type: DataTypes.STRING, allowNull: false},
+  },
+  {sequelize, tableName: 'messages'}
+);
--- src/models/message/message.type.ts
+++ src/models/message/message.type.ts
@@ -2,9 +2,8 @@ import {Optional} from 'sequelize';
 
 export type MessageAttributes = {
   id: string;
-  identifier: string;
+  idHash: string; // a simple hash to be able to query messages fast i.e concatenated sorted id's on message participants
   text: string;
-  senderId: string;
 };
 
-export type UserCreationAttributes = Optional<MessageAttributes, 'id'>;
+export type MessageCreationAttributes = Optional<MessageAttributes, 'id'>;
