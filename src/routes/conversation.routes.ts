0d94ecf28e093e0f24d31e1d606b55b719c0db0e
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-16 14:15:33 +0000

    add conversation routes

---
+++ src/routes/conversation.routes.ts
@@ -0,0 +1,16 @@
+// under normal circumstances.... I feel there should be no route for conversations
+// a conversation instance should be created when a match is made and should be handled by the backend
+// not called from the frontend.
+// with that out of the way, it's needed for testing purposes
+
+// TODO: remove routes after matching algo's are completed
+
+import {Router} from 'express';
+import conversationController from '../controller/conversation.controller';
+
+const router = Router();
+
+router.get('/:id', conversationController.get);
+router.post('/:id', conversationController.set);
+
+export default router;
