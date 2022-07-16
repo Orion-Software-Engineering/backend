00bb06786fe5db39b55390a823b10f2b4c5417c7
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-16 17:34:51 +0000

    modified message attributes

--- src/models/message/message.type.ts
+++ src/models/message/message.type.ts
@@ -4,6 +4,7 @@ export type MessageAttributes = {
   id: string;
   idHash: string; // a simple hash to be able to query messages fast i.e concatenated sorted id's on message participants
   text: string;
+  senderId?: string;
 };
 
 export type MessageCreationAttributes = Optional<MessageAttributes, 'id'>;
