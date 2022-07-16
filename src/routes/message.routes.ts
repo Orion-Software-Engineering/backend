95f0106ea4d572fb38f63d84b7b1057c0732f5b1
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-07-16 17:36:16 +0000

    add message get route

--- src/routes/message.routes.ts
+++ src/routes/message.routes.ts
@@ -4,5 +4,6 @@ import messageController from '../controller/message.controller';
 const router = Router();
 
 router.post('/', messageController.add);
+router.get('/:senderId/:receiverId', messageController.getMessages);
 
 export default router;
