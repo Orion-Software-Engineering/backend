36dba435a1456199ba990377f867b4a1934de5ea
Max <maxotuteye12@gmail.com>
2022-07-22 21:36:19 +0000

    suddenly got hit with prettier errors. fixed

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 const app = express();
 const corsOptions = {
-  origin: 'https://localhost:8000',
+    origin: 'https://localhost:8000',
 };
 
 app.use(cors());
@@ -26,29 +26,29 @@ app.use(bodyParser.urlencoded({extended: true}));
 const {Role, Interest, INTERESTS, ROLES} = db;
 
 sequelize
-  .sync({force: true}) // force: true forces dropping and resyncing the database
-  .then(() => {
-    console.log('Syncing DB');
-    initial();
-  });
+    .sync({force: true}) // force: true forces dropping and resyncing the database
+    .then(() => {
+        console.log('Syncing DB');
+        initial();
+    });
 
 // this function initializes the roles, run only once on a new database else there'll be errors
 function initial() {
-  ROLES.forEach(role => {
-    Role.create({
-      name: role,
+    ROLES.forEach(role => {
+        Role.create({
+            name: role,
+        });
     });
-  });
 
-  INTERESTS.forEach(interest => {
-    Interest.create({
-      name: interest,
+    INTERESTS.forEach(interest => {
+        Interest.create({
+            name: interest,
+        });
     });
-  });
 }
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({message: 'Welcome to Orion Meet'});
+    res.json({message: 'Welcome to Orion Meet'});
 });
 
 userRoutes(app);
@@ -64,6 +64,6 @@ app.use('/api/conversation', conversationRouter);
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`DATABASE_URL is ${process.env.DATABASE_URL}`);
+    console.log(`Server running on port ${PORT}`);
+    console.log(`DATABASE_URL is ${process.env.DATABASE_URL}`);
 });
