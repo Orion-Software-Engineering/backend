65b5bd8cb21793ab3efa8f36a5bd5f378abdf7b1
Max <maxotuteye12@gmail.com>
2022-08-09 23:47:26 +0000

    added addmessage controller

--- src/controller/chat/conversation.message.controller.ts
+++ src/controller/chat/conversation.message.controller.ts
@@ -4,16 +4,29 @@ import db from '../../models';
 const {Conversation, Message} = db;
 
 export const addMessageToConversation = async (req: Request, res: Response) => {
-    const {userId, messageText, conversationId} = req.body
+    try {
+        const {userId, messageText, conversationId} = req.body
 
-    const conversation = await Conversation.findByPk(conversationId)
-    const message = await Message.create({text: messageText})
+        const conversation = await Conversation.findByPk(conversationId)
+        const message = await Message.create({text: messageText})
 
-    await conversation?.addMessage([message.id])
+        await conversation?.addMessage([message.id])
+
+        return res.status(200).send()
+    } catch (e) {
+        return res.status(400).send()
+    }
 }
 
 export const removeMessageFromConversation = async (req: Request, res: Response) => {
+    try {
+        const {userId, messageId, conversationId} = req.body
 
+        const conversation = await Conversation.findByPk(conversationId)
+        conversation?.removeMessage([messageId])
+    } catch (e) {
+        return res.status(400).send()
+    }
 }
 
 export const getMessageFromConversation = async (req: Request, res: Response) => {
