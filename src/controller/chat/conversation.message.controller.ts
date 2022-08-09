355e482299af52da68cbe7eed661d48e80b77038
Max <maxotuteye12@gmail.com>
2022-08-09 23:19:51 +0000

    adding controllers for getting messages from conversations

---
+++ src/controller/chat/conversation.message.controller.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from "express";
+import db from '../../models';
+
+const {Conversation, Message} = db;
+
+export const addMessageToConversation = async (req: Request, res: Response) => {
+
+}
+
+export const removeMessageFromConversation = async (req: Request, res: Response) => {
+
+}
+
+export const getMessageFromConversation = async (req: Request, res: Response) => {
+
+}
+
+export const getMessagesFromConversation = async (req: Request, res: Response) => {
+
+}
