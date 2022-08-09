8ca4f321863a3b07b3eafecc1abe869cdc2ed167
Max <maxotuteye12@gmail.com>
2022-08-09 23:17:35 +0000

    added controllers for adding and removing conversations

---
+++ src/controller/chat/user.conversation.controller.ts
@@ -0,0 +1,30 @@
+import {Request, Response} from "express";
+import db from '../../models';
+
+const {User} = db;
+
+export const addConversationToUser = async (req: Request, res: Response) => {
+    try {
+        const {userId, conversationId} = req.body
+        const user = await User.findByPk(userId)
+
+        await user?.addConversations([conversationId])
+
+        return res.status(201).send('conversations added successfully')
+    } catch (e) {
+        return res.status(400).send({e})
+    }
+}
+
+export const removeConversationFromUser = async (req: Request, res: Response) => {
+    try {
+        const {userId, conversationId} = req.body
+        const user = await User.findByPk(userId)
+
+        await user?.removeConversations([conversationId])
+
+        return res.status(200).send('conversations removed successfully')
+    } catch (e) {
+        res.status(400).send({e})
+    }
+}
