d834a9bbc5efef75cf8b36bac28bb088cf0235f1
McSam-py <ardisam9876@gmail.com>
2022-08-12 17:22:56 +0300

    created createEvent route

--- src/routes/events.routes.ts
+++ src/routes/events.routes.ts
@@ -1,1 +1,16 @@
 import {Express, Request, Response} from 'express';
+import * as controller from '../controller/events.controller';
+import isModerator from "../middleware/authentication/isModerator";
+
+
+export default (app: Express) => {
+    app.use((req: Request, res: Response, next: Function) => {
+        res.header(
+            'Access-Control-Allow-Headers',
+            'x-access-token, Origin, Content-Type, Accept'
+        );
+        next();
+    });
+
+    app.post("/createEvent", [isModerator], controller.createEvents);
+}
