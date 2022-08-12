4cb1dc7fd0ea94979007f76117f7750bf3889434
McSam-py <ardisam9876@gmail.com>
2022-08-12 17:13:59 +0300

    added controller for creating, deleting and retrieving events

---
+++ src/controller/events.controller.ts
@@ -0,0 +1,20 @@
+import {Request, Response} from 'express';
+import Sequelize from 'sequelize';
+import db from '../models';
+import config from '../config/auth.config';
+
+
+// Module for allowing users with moderator access to create events
+export const createEvents = async (req: Request, res: Response) => {
+
+};
+
+// Module for getting current events
+export const getEvents = async (req: Request, res: Response) => {
+
+};
+
+// Module for deleting events
+export const deleteEvents = async (req: Request, res: Response) => {
+
+};
