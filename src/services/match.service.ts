adc1096950f701396217c650bfc5cc12f2aad3ae
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-17 17:20:33 +0000

    layout structure for match finding

---
+++ src/services/match.service.ts
@@ -0,0 +1,9 @@
+const findMatches = () => {
+  //  find interest of user
+  // find interests users with at most one shared interest
+  // remove duplicates
+  // get interest of each user in unique array
+  // order them in order of most shared interests
+};
+
+export {findMatches};
