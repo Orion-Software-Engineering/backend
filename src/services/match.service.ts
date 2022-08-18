9148c6732ca9928332456636862893a97395042f
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-18 19:56:23 +0000

    get interesst and users with shared interests

--- src/services/match.service.ts
+++ src/services/match.service.ts
@@ -1,9 +1,21 @@
-const findMatches = () => {
-  //  find interest of user
+import db, {sequelize} from '../models';
+import interestService from './interest.service';
+
+const findMatches = async (id: string) => {
+  //  find interests of user
+  const interests = await interestService.get(id);
+  const interestIds = interests?.map(interest => {
+    return interest.id;
+  }) as string[];
   // find interests users with at most one shared interest
-  // remove duplicates
+
+  const [result] = await sequelize.query(
+    'SELECT DISTINCT "userId" FROM user_interests WHERE "roleId" in (:interests) AND NOT "userId" = :user LIMIT 20',
+    {replacements: {interests: interestIds, user: id}}
+  );
   // get interest of each user in unique array
   // order them in order of most shared interests
+  return result;
 };
 
-export {findMatches};
+export default {findMatches};
