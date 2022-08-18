2c070364243ce70e595d7960fa50c52e0ae8cf30
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-08-18 19:56:56 +0000

    add match route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,42 +1,36 @@
 import {NextFunction, Request, Response, Express} from 'express';
 
 import * as controller from '../controller/user.controller';
-import verifyToken from "../middleware/authentication/verifyToken";
-import isModerator from "../middleware/authentication/isModerator";
-import isAdmin from "../middleware/authentication/isAdmin";
+import * as matchController from '../controller/match.controller';
+import verifyToken from '../middleware/authentication/verifyToken';
+import isModerator from '../middleware/authentication/isModerator';
+import isAdmin from '../middleware/authentication/isAdmin';
 
 // routes for user functions
 
 export default (app: Express) => {
-    app.use((req: Request, res: Response, next: NextFunction) => {
-        res.header(
-            'Access-Control-Allow-Headers',
-            'x-access-token, Origin, Content-Type, Accept'
-        );
-        next();
-    });
-
-    app.get('/api/test/all', controller.allAccess);
-
-    app.get('/api/test/user',
-        [verifyToken],
-        controller.userBoard);
-
-    app.get(
-        '/api/test/mod',
-        [verifyToken, isModerator],
-        controller.moderatorBoard
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header(
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
     );
+    next();
+  });
 
-    app.get(
-        '/api/test/admin',
-        [verifyToken, isAdmin],
-        controller.adminBoard
-    );
+  app.get('/api/test/all', controller.allAccess);
 
-    app.get(
-        '/api/test/users',
-        [verifyToken, isAdmin],
-        controller.showAll
-    );
+  app.get('/api/test/user', [verifyToken], controller.userBoard);
+
+  app.get(
+    '/api/test/mod',
+    [verifyToken, isModerator],
+    controller.moderatorBoard
+  );
+
+  app.get('/api/test/admin', [verifyToken, isAdmin], controller.adminBoard);
+
+  app.get('/api/test/users', [verifyToken, isAdmin], controller.showAll);
+
+  // TODO: id param should be removed and verifyToke6n6 6m6iddleware should be inclueded
+  app.get('/api/test/matches/:id', matchController.find);
 };
