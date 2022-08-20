2658bf7b929b35f18119a476678c63cf413969ee
Max <maxotuteye12@gmail.com>
2022-08-20 12:21:24 +0000

    disabling husky for now

--- src/models/event/event.model.ts
+++ src/models/event/event.model.ts
@@ -4,80 +4,72 @@ import {InterestAttributes} from '../interest';
 import {EventAttributes, EventCreationAttributes} from './event.type';
 
 // define database model for events
-export default class Event extends Model<
-  EventAttributes,
-  EventCreationAttributes
-> {
-  declare id: CreationOptional<string>;
-  declare name: String;
-  declare description: string;
-  declare date: Date;
-  declare venue: string;
-  declare organizers: string;
-  declare mcs: string;
-  declare guests: string;
-  declare age_restriction: boolean;
+export default class Event extends Model<EventAttributes,
+    EventCreationAttributes> {
+    declare id: CreationOptional<string>;
+    declare name: String;
+    declare description: string;
+    declare date: Date;
+    declare venue: string;
+    declare organizers: string;
+    declare mcs: string;
+    declare guests: string;
+    declare age_restriction: boolean;
 
-  declare getInterests: Sequelize.BelongsToManyGetAssociationsMixin<InterestAttributes>;
-  declare setInterests: Sequelize.BelongsToManySetAssociationsMixin<
-    InterestAttributes,
-    InterestAttributes['id']
-  >;
-  declare addInterests: Sequelize.BelongsToManyAddAssociationsMixin<
-    InterestAttributes,
-    InterestAttributes['id']
-  >;
-  declare removeInterests: Sequelize.BelongsToManyRemoveAssociationsMixin<
-    InterestAttributes,
-    InterestAttributes['id']
-  >;
+    declare getInterests: Sequelize.BelongsToManyGetAssociationsMixin<InterestAttributes>;
+    declare setInterests: Sequelize.BelongsToManySetAssociationsMixin<InterestAttributes,
+        InterestAttributes['id']>;
+    declare addInterests: Sequelize.BelongsToManyAddAssociationsMixin<InterestAttributes,
+        InterestAttributes['id']>;
+    declare removeInterests: Sequelize.BelongsToManyRemoveAssociationsMixin<InterestAttributes,
+        InterestAttributes['id']>;
 }
 
 Event.init(
-  {
-    name: {
-      type: DataTypes.STRING,
-      defaultValue: DataTypes.STRING,
-      allowNull: true,
+    {
+        name: {
+            type: DataTypes.STRING,
+            defaultValue: DataTypes.STRING,
+            allowNull: true,
+        },
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        venue: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        organizers: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        mcs: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        guests: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        age_restriction: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-      allowNull: false,
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false,
-    },
-    venue: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    organizers: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    mcs: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    guests: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    age_restriction: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-      allowNull: false,
-    },
-    description: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: 'events',
-  }
+    {
+        sequelize,
+        tableName: 'events',
+    }
 );
--- src/models/event/event.type.ts
+++ src/models/event/event.type.ts
@@ -1,15 +1,15 @@
 import {Optional} from 'sequelize';
 
-export type Attributes = {
-  id: string;
-  name: string;
-  description: string;
-  date: Date;
-  venue: string;
-  organizers: string;
-  mcs: string;
-  guests: string;
-  age_restriction: number;
+export type EventAttributes = {
+    id: string;
+    name: string;
+    description: string;
+    date: Date;
+    venue: string;
+    organizers: string;
+    mcs: string;
+    guests: string;
+    age_restriction: number;
 };
 
-export type CreationAttributes = Optional<Attributes, 'id' | 'guests' | 'mcs'>;
+export type EventCreationAttributes = Optional<EventAttributes, 'id' | 'guests' | 'mcs'>;
