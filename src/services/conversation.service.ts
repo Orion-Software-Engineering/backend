a429706272dc32032b670eee62a93bb660bd8b08
Max <maxotuteye12@gmail.com>
2022-08-21 04:17:08 +0000

    finished events, at least for now

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -1,23 +1,23 @@
 import db from '../models';
 
 const get = async (id: string) => {
-  return await db.Conversation.findAll({
-    where: {
-      userId: id,
-    },
-  });
+    return await db.Conversation.findAll({
+        where: {
+            userId: id,
+        },
+    });
 };
 
 const add = async (userId: string, senderId: string) => {
-  // two conversations need to be created; one for the sender and another for the recipient
-  await db.Conversation.create({
-    senderId,
-    userId,
-  });
-  await db.Conversation.create({
-    senderId: userId,
-    userId: senderId,
-  });
+    // two conversations need to be created; one for the sender and another for the recipient
+    await db.Conversation.create({
+        senderId,
+        userId,
+    });
+    await db.Conversation.create({
+        senderId: userId,
+        userId: senderId,
+    });
 };
 
 export default {get, add};
