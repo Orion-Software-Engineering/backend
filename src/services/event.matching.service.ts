4e7d53e5cb846d424a7c6d1575c247c2c7205ae6
Sven Dzeble <svendzeble@outlook.com>
2022-08-24 01:48:34 +0100

    created function for getting all event IDs

--- src/services/event.matching.service.ts
+++ src/services/event.matching.service.ts
@@ -1,1 +1,14 @@
+import db from '../models';
+import Interest from "../models/interest";
+// import {sequelize} from '..';
+import Event from "../models/event";
 
+export const getAllEventIds = async () => {
+    const event =  await db.Event.findAll({
+        attributes: ['id']
+    });
+     if (event){
+         return event;
+     }
+
+};
