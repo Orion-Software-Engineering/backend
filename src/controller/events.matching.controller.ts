bc9b0ee4c99111786aa646b43b0f84bda3013934
McSam-py <ardisam9876@gmail.com>
2022-08-31 02:06:43 +0300

    events.matching controller created and working. Off to correct some logical errors in event.matching.service

--- src/controller/events.matching.controller.ts
+++ src/controller/events.matching.controller.ts
@@ -1,8 +1,23 @@
 import {Request,Response} from "express";
 import {eventMatch} from "../services/event.matching.service";
+import db from "../models";
 
 export const  eventsMatchingController = async (req: Request, res: Response) => {
     const {id} = req.params;
 
-    await eventMatch(id);
+    const events: object[] = [];
+
+    const eventsAndPriorities = await eventMatch(id);
+
+    if (eventsAndPriorities){
+        for (const [value, key] of eventsAndPriorities){
+           const event = await db.Event.findByPk(key);
+
+           if (event){
+               events.push(event)
+           }
+        }
+        return res.send(events)
+    }
+   return res.send("Message: Couldn't do user matching");
 }
