6679113489cf549c0080819f97d0caac3c7d40d9
Max <maxotuteye12@gmail.com>
2022-08-31 00:59:49 +0000

    addressing some error handling

--- src/controller/match.controller.ts
+++ src/controller/match.controller.ts
@@ -2,6 +2,11 @@ import {Request, Response} from 'express';
 import matchService from '../services/match.service';
 
 export const find = async (req: Request, res: Response) => {
-  const result = await matchService.findMatches(req.params.id);
-  res.status(200).send({data: result});
+    try {
+        const result = await matchService.findMatches(req.params.id);
+        res.status(200).send({data: result});
+    } catch ({message}) {
+        console.log(message)
+        return res.status(400).send('Unable to find matches.')
+    }
 };
