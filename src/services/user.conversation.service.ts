240e44eaff10e0d99b764c1ac1a6a0281d2612d8
Max Otuteye <maxotuteye12@gmail.com>
2022-09-27 02:27:49 +0000

    altering get conversations of user to reduce number of calls required on app

--- src/services/user.conversation.service.ts
+++ src/services/user.conversation.service.ts
@@ -28,25 +28,81 @@ const getUsers = async (conversationId: string) => {
 }
 
 const getUserConversations = async (userId: string) => {
-    return await Conversation.findAll({
-        include: [
-            {
-                model: User,
-                as: "Users",
-                where: {
-                    id: userId
-                },
-                attributes: []  // we don't need any attributes from user
+    // return await Conversation.findAll({
+    //     include: [
+    //         {
+    //             model: User,
+    //             as: "Users",
+    //             where: {
+    //                 id: userId
+    //             },
+    //             attributes: []  // we don't need any attributes from user
+    //         },
+    //         {
+    //             model: Message,
+    //             as: "Messages",
+    //             attributes: [],
+    //         }],
+    //     attributes: ['id'],
+    //     order: [['Messages', 'createdAt', 'DESC']]
+    // })
+
+    const conversations = await Conversation.findAll({
+        include: [{
+            model: User,
+            where: {
+                id: userId
             },
-            {
-                model: Message,
-                as: "Messages",
-                attributes: [],
-            }],
+            attributes: []
+        }, {
+            model: Message,
+            as: "Messages",
+            attributes: [],
+        }],
         attributes: ['id'],
         order: [['Messages', 'createdAt', 'DESC']]
     })
-    // return user?.getConversations()
+
+    const conversationsWithLastMessages: any[] = []
+    for (const conversation of conversations) {
+        const lastMessage = await getLastMessageOfConversation(conversation.id)
+        const users = await User.findAll({
+            include: [{
+                model: Conversation,
+                where: {
+                    id: conversation.id
+                },
+                attributes: []
+            }],
+            attributes: ['id', 'username']
+        })
+        const tempCon = conversation
+        // @ts-ignore
+        tempCon.setDataValue('lastMessage', lastMessage)
+
+        // @ts-ignore
+        tempCon.setDataValue('users', users)
+        conversationsWithLastMessages.push(tempCon)
+    }
+
+    return conversationsWithLastMessages
+}
+
+const getLastMessageOfConversation = async (conversationId: string) => {
+    const message = await Message.findOne({
+        where: {
+            conversationId: conversationId
+        },
+        attributes: ['userId', 'text'],
+        order: [['createdAt', 'DESC']]
+    })
+    if (!message) throw new Error("No message found")
+
+    const sender = await User.findByPk(message.userId).then(user => user?.username)
+
+    // @ts-ignore
+    message.setDataValue('sender', sender)
+    return message
 }
 
 export default {
