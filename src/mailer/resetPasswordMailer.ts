26eed16bc2819d06db1c6f80098e1c9b87db9d2d
Max Otuteye <maxotuteye12@gmail.com>
2022-09-28 16:17:57 +0000

    keep changes

--- src/mailer/resetPasswordMailer.ts
+++ src/mailer/resetPasswordMailer.ts
@@ -2,30 +2,35 @@ const sib = require('sib-api-v3-sdk')
 require('dotenv').config()
 
 export const sendResetMail = (email: string, resetPasswordLink: string) => {
-    const defaultClient = sib.ApiClient.instance;
-    const apiKey = defaultClient.authentications['api-key'];
-    apiKey.apiKey = process.env.SMTP_API_KEY;
-    const apiInstance = new sib.TransactionalEmailsApi();
-    const sendSmtpEmail = new sib.SendSmtpEmail();
+    try {
+        const defaultClient = sib.ApiClient.instance;
+        const apiKey = defaultClient.authentications['api-key'];
+        apiKey.apiKey = process.env.SMTP_API_KEY;
+        const apiInstance = new sib.TransactionalEmailsApi();
+        const sendSmtpEmail = new sib.SendSmtpEmail();
 
-    sendSmtpEmail.subject = 'Password Reset.';
-    sendSmtpEmail.htmlContent =
-        '<html lang="en">' +
-        '<body>' +
-        '<h1>Reset Password.</h1>' +
-        '<a href={{params.parameter}}>Click here to Reset Password</a>' +
-        '</body>' +
-        '</html>';
-    sendSmtpEmail.sender = {'name': 'Orion', 'email': 'support@orion.com'};
-    sendSmtpEmail.to = [{'email': email}];
-    sendSmtpEmail.replyTo = {'email': 'noreply@orion.com', 'name': 'noreply'};
-    sendSmtpEmail.headers = {'Orion-Email-Reset': 'unique-id-1234'};
+        sendSmtpEmail.subject = 'Password Reset.';
+        sendSmtpEmail.htmlContent =
+            '<html lang="en">' +
+            '<body>' +
+            '<h1>Reset Password.</h1>' +
+            '<a href={{params.parameter}}>Click here to Reset Password</a>' +
+            '</body>' +
+            '</html>';
+        sendSmtpEmail.sender = {'name': 'Orion', 'email': 'support@orion.com'};
+        sendSmtpEmail.to = [{'email': email}];
+        sendSmtpEmail.replyTo = {'email': 'noreply@orion.com', 'name': 'noreply'};
+        sendSmtpEmail.headers = {'Orion-Email-Reset': 'unique-id-1234'};
 
-    sendSmtpEmail.params = {'parameter': resetPasswordLink, 'subject': 'Reset Password.'};
+        sendSmtpEmail.params = {'parameter': resetPasswordLink, 'subject': 'Reset Password.'};
 
-    apiInstance.sendTransacEmail(sendSmtpEmail).then((data: any) => {
-        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-    }, (error: any) => {
-        console.error(error);
-    });
+        apiInstance.sendTransacEmail(sendSmtpEmail).then((data: any) => {
+            console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        }, (error: any) => {
+            console.error(error);
+        });
+    } catch ({message}) {
+        console.log(message)
+    }
+
 };
